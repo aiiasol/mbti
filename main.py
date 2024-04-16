d46e56273bc91db09bf44948664c02666ac7e6c5
aiiasol <162348901+aiiasol@users.noreply.github.com>
2024-04-16 14:09:10 +0100

    Update main.py

--- main.py
+++ main.py
@@ -13,3 +13,12 @@ if st.button('오늘의 운세보기'):
     with st.spinner('운세 보는 중...'):
         result = llm.predict(content + "의 오늘의 운세를 봐 줘")
         st.write(result)
+
+
+
+# Advertisement iframe
+advertisement_iframe = """
+<iframe src="https://ads-partners.coupang.com/widgets.html?id=769781&template=carousel&trackingCode=AF7086871&subId=&width=680&height=140&tsource=" width="680" height="140" frameborder="0" scrolling="no" referrerpolicy="unsafe-url" browsingtopics></iframe>
+"""
+
+st.markdown(advertisement_iframe, unsafe_allow_html=True)
